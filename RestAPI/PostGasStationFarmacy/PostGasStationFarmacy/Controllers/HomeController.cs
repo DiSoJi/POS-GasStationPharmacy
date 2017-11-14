1c258d56f27263f255088536ab815de23fb06309
DiSoJi <diegosolisj@gmail.com>
2017-11-14 01:27:06 -0600

    recommit, comportamiento extraño del git

---
+++ RestAPI/PostGasStationFarmacy/PostGasStationFarmacy/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostGasStationFarmacy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string currentTime = DateTime.Now.ToLongTimeString();
+            ViewBag.Message = "The current time is " + currentTime;
+            return View();
+        }
+    }
+}
---
+++ RestAPI/PostGasStationFarmacy/PostGasStationFarmacy/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using PostGasStationFarmacy.Models;
+//using EntityFramework;
+
+
+namespace PostGasStationFarmacy.Controllers
+{
+    public class TestController : ApiController
+    {
+        //public IEnumerable<test>
+        public JObject Get()
+        {
+            JObject response =
+                new JObject(
+                new JProperty("Code", 200),
+                new JProperty("Description", "Succesfull")
+                );
+            return response;
+        }
+
+        public JObject Post(int x)
+        {
+            Class1 test = new Class1();
+            JObject response =  test.Test(x);
+            return response;
+        }
+    }
+}
